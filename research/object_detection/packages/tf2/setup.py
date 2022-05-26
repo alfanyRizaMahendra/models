a2e7e3557d4a7af6c5d3d78ae78bce23b9a3bcab
Alfany Riza Mahendra <70315533+alfanyRizaMahendra@users.noreply.github.com>
2022-05-26 18:57:49 +0700

    Update setup.py

--- research/object_detection/packages/tf2/setup.py
+++ research/object_detection/packages/tf2/setup.py
@@ -18,8 +18,9 @@ REQUIRED_PACKAGES = [
     'lvis',
     'scipy',
     'pandas',
-    'tf-models-official>=2.5.1',
-    'tensorflow_io',
+    'tensorflow==2.6.0',
+    'tf-models-official==2.6.0',
+    'tensorflow_io==0.23.1',
     'keras',
     'pyparsing==2.4.7'  # TODO(b/204103388)
 ]
